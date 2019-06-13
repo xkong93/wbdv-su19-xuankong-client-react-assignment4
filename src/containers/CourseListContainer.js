411cc7502e80e715042fea9621d5c6934451e831
xkong <kong.xu@husky.meu.edu>
2019-06-13 14:58:45 -0700

    changed deleteRow

--- src/containers/CourseListContainer.js
+++ src/containers/CourseListContainer.js
@@ -3,47 +3,54 @@ import CourseListComponent from "../components/CourseListComponent";
 import CourseService from "../services/CourseService";
 import NavBarAndTableHead from "../components/NavBarAndTableHeader";
 import CourseGrid from "../components/CourseGrid";
+
 export default class CourseListContainer extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    //new an obj
-    this.courseService = CourseService.getInstance();
-    this.state = {
-      courses: [],
-      toggle: false,
-    };
-    console.log(this.state.courses)
-    this.onclickChangeView = this.onclickChangeView.bind(this);
-  }
+        //new an obj
+        this.courseService = CourseService.getInstance();
+        this.state = {
+            courses: [],
+            toggle: false,
+        };
+        console.log(this.state.courses)
+        this.onclickChangeView = this.onclickChangeView.bind(this);
+    }
 
 
-  componentDidMount() {
-      this.courseService.findAllCourses()
-          .then(courses => this.setState({
-              courses:courses,
-          }))
-  }
+    componentDidMount() {
+        this.courseService.findAllCourses()
+            .then(courses => this.setState({
+                courses: courses,
+            }))
+    }
 
-    onclickChangeView(){
-    this.setState({
-        toggle: !this.state.toggle
-    })
- }
-
-      deleteRow = (id) => {
+    onclickChangeView() {
         this.setState({
-            courses:this.state.courses.filter(course => course.id != id)
+            toggle: !this.state.toggle
         })
     }
 
-  render() {
-    return (
-      <div>
-        <NavBarAndTableHead changeView={this.onclickChangeView}/>
-        {this.state.toggle ? <CourseGrid deleteRow={this.deleteRow} courses={this.state.courses} />: <CourseListComponent deleteRow={this.deleteRow} 
-        courses={this.state.courses} /> }
-      </div>
-    );
-  }
+    deleteRow = (id) => {
+        // this.setState({
+        //     courses: this.state.courses.filter(course => course.id != id)
+        // })
+        this.courseService.deleteCourse(id)
+            .then(this.courseService.findAllCourses()
+                .then(courses => this.setState({
+                    courses: courses
+                })))
+    }
+
+    render() {
+        return (
+            <div>
+                <NavBarAndTableHead changeView={this.onclickChangeView}/>
+                {this.state.toggle ? <CourseGrid deleteRow={this.deleteRow} courses={this.state.courses}/> :
+                    <CourseListComponent deleteRow={this.deleteRow}
+                                         courses={this.state.courses}/>}
+            </div>
+        );
+    }
 }
